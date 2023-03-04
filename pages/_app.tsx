f44e1acd67484aff2f5b7f48904d1f46efb8c824
othmane ait taleb <aittaleb108@gmail.com>
2023-03-04 17:21:58 +0100

    feature: adding check out page with 4 steps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+// @ts-nocheck
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { StoreProvider } from "../utils/Store";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import React from "react";
+import { useRouter } from "next/router";
 
 export default function App({
   Component,
@@ -10,8 +13,29 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth>
+            <Component />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </StoreProvider>
     </SessionProvider>
   );
+}
+
+function Auth({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+    },
+  });
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 }
--- utils/Store.js
+++ utils/Store.js
@@ -1,37 +1,55 @@
 import React, { createContext } from "react";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
-export const Store = createContext()
+export const Store = createContext();
 
 const initialState = {
-    cart: Cookies.get('cart') ? JSON.parse(Cookies.get('cart')) : { cartItems: [] },
-}
+  cart: Cookies.get("cart")
+    ? JSON.parse(Cookies.get("cart"))
+    : { cartItems: [], shippingAddress: {} },
+};
 
 function reducer(state, action) {
-    switch(action.type)
-    {
-        case 'CART_ADD_ITEM': {
-            const newItem = action.payload;
-            const existItem = state.cart.cartItems.find(item => item.id === newItem.id);
-            const cartItems = existItem
-            ? state.cart.cartItems.map(item => item.id === existItem.id ? newItem : item)
-            : [...state.cart.cartItems, newItem];
-            Cookies.set('cart',JSON.stringify({ cartItems }))
-            return { ...state, cart: { cartItems } };
-        }
-        case 'CART_REMOVE_ITEM': {
-            const cartItems = state.cart.cartItems.filter(item => item.id !== action.payload.id);
-            Cookies.set('cart',JSON.stringify({ cartItems }))
-
-             return { ...state, cart: {cartItems } };
-        }
-        default:
-            return state;
+  switch (action.type) {
+    case "CART_ADD_ITEM": {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item.id === newItem.id
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            item.id === existItem.id ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      Cookies.set("cart", JSON.stringify({ cartItems }));
+      return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case "CART_REMOVE_ITEM": {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      Cookies.set("cart", JSON.stringify({ cartItems }));
+
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
+        },
+      };
+    default:
+      return state;
+  }
 }
 
-export function StoreProvider({children}) {
-    const [state, dispatch] = React.useReducer(reducer, initialState);
-    const value = { state, dispatch };
-    return <Store.Provider value={value}>{children}</Store.Provider>
+export function StoreProvider({ children }) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{children}</Store.Provider>;
 }
